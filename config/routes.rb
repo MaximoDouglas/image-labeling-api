64257e9e7999c9c140cfb86d6ecdda8c87ce0d7a
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-09-09 04:02:05 -0300

    Add routes to get image classes for a domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :image_classes
-  resources :domains
+  
+  resources :domains do 
+    resources :image_classes
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
